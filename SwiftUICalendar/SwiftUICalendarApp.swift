47322ee219b8f2b2f5a237d215439038d3e04ba0
Kyle Peterson <kdpeters@gmail.com>
2022-07-28 08:18:09 -0400

    Initial Commit

---
+++ SwiftUICalendar/SwiftUICalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICalendarApp.swift
+//  SwiftUICalendar
+//
+//  Created by Kyle Peterson on 7/28/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
